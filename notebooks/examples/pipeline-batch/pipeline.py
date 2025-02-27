8f93d2d9c9e0d530640325c917faef827c8567b4
Joseph Dong <josephdong1000@gmail.com>
2025-02-27 15:50:58 -0500

    Update notebook examples and pipeline scripts
    - Removed deprecated pipeline.ipynb
    - Added new pipeline.py script for batch processing
    - Added corresponding pipeline.sh shell script for running batch jobs
    - Updated pythoneeg_figures.ipynb with minor changes to animal IDs
    - Enhanced test_all_functions.ipynb with additional visualization methods

---
+++ notebooks/examples/pipeline-batch/pipeline.py
@@ -0,0 +1,42 @@
+# %% [markdown]
+# # Marsh Lab Analysis Pipeline
+
+# %%
+import sys
+from pathlib import Path
+
+packageroot = Path('/mnt/isilon/marsh_single_unit/PythonEEG')
+print(packageroot)
+sys.path.append(str(packageroot))
+
+# %%
+import tempfile
+from tqdm import tqdm
+
+from pythoneeg import core
+from pythoneeg import visualization
+from pythoneeg import constants
+
+tempfile.tempdir = '/scr1/users/dongjp'
+tqdm.set_defaults(miniters=500)
+
+# %%
+base_folder = Path('/mnt/isilon/marsh_single_unit/PythonEEG Data Bins')
+output_folder = Path(__file__).parent.parent.resolve() / 'pipeline-wars'
+animal_ids = ['A5 WT', 'A10 KO', 'F22 KO', 'G25', 'G26', 'N21', 'N22', 'N23', 'N24', 'N25']
+
+# %%
+for animal_id in animal_ids:
+    ao = visualization.AnimalOrganizer(base_folder, animal_id, mode="concat", assume_from_number=True)
+    ao.convert_colbins_to_rowbins()
+    ao.convert_rowbins_to_rec()
+
+    war = ao.compute_windowed_analysis(['all'], exclude=['nspike', 'wavetemp'], multiprocess_mode='dask')
+    war.to_pickle_and_json(output_folder / animal_id)
+
+
+# %%
+# Run in batch mode
+"""
+sbatch --mem 50G -c 20 -t 24:00:00 /mnt/isilon/marsh_single_unit/PythonEEG/notebooks/examples/pipeline-batch/pipeline.sh 
+"""
---
+++ notebooks/examples/pipeline-batch/pipeline.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+module load Python/3.10.8-GCCcore-12.2.0.lua
+cd /mnt/isilon/marsh_single_unit/PythonEEG
+source .venv-linux-3.10/bin/activate
+
+python -u /mnt/isilon/marsh_single_unit/PythonEEG/notebooks/examples/pipeline-batch/pipeline.py
+
+echo "Pipeline finished."
