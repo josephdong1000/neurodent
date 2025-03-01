9190e1a7256360a7d1e14b45307d2839175c3cf6
Joseph Dong <josephdong1000@gmail.com>
2025-03-01 18:56:50 -0500

    copy pipeline.py to test parallel processing

--- notebooks/examples/pipeline-batch/pipeline-parallel-testing.py
+++ notebooks/examples/pipeline-batch/pipeline-parallel-testing.py
@@ -4,56 +4,66 @@
 # %%
 import sys
 from pathlib import Path
-import dask
-import logging
+import tempfile
 import os
+import logging
+import time
+import subprocess
 
-# Set up logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger('dask')
-logger.setLevel(logging.DEBUG)
+import dask
+from dask.distributed import Client, LocalCluster
+from dask_jobqueue.slurm import SLURMRunner, SLURMCluster
 
 packageroot = Path('/mnt/isilon/marsh_single_unit/PythonEEG')
 print(packageroot)
 sys.path.append(str(packageroot))
-print("pingusball")
 
-# Log Dask/Slurm info
-# logger.info(f"Number of Slurm cores: {os.environ.get('SLURM_CPUS_PER_TASK')}")
-# logger.info("Current Dask configuration:")
-# for key, value in dask.config.config.items():
-#     logger.info(f"  {key}: {value}")
+from pythoneeg import core  # noqa: E402
+from pythoneeg import visualization  # noqa: E402
+from pythoneeg import constants  # noqa: E402
 
 
+if __name__ == '__main__':
 
-# %%
-import tempfile
-
-from pythoneeg import core
-from pythoneeg import visualization
-from pythoneeg import constants
-
-tempfile.tempdir = '/scr1/users/dongjp'
-
-# %%
-base_folder = Path('/mnt/isilon/marsh_single_unit/PythonEEG Data Bins')
-output_folder = Path(__file__).parent.parent.resolve() / 'pipeline-wars'
-animal_ids = ['A5 WT', 'A10 KO', 'F22 KO']
-
-# %%
+    # Set up logging
+    # logging.basicConfig(level=logging.INFO)
+    # logger = logging.getLogger('dask')
+    # logger.setLevel(logging.DEBUG)
 
 
+    # print(f"portdash: {portdash}")
 
-for animal_id in animal_ids:
-    ao = visualization.AnimalOrganizer(base_folder, animal_id, mode="concat", assume_from_number=True)
-    ao.convert_colbins_to_rowbins()
-    ao.convert_rowbins_to_rec()
+    # raise Exception("Stop here")
 
-    war = ao.compute_windowed_analysis(['all'], exclude=['nspike', 'wavetemp'], multiprocess_mode='dask-slurm')
-    # war.to_pickle_and_json(output_folder / animal_id)
+    cluster = SLURMCluster(cores=5, memory='20GB')
+    cluster.scale(jobs=20)
+    client = Client(cluster)
 
-# %%
-# Run in batch mode
+    # cluster = LocalCluster(n_workers=1, threads_per_worker=1)
+    # client = Client(cluster)
+
+    print(f"\n\n\tclient.dashboard_link: {client.dashboard_link}\n\n")
+
+    tempfile.tempdir = '/scr1/users/dongjp'
+
+    base_folder = Path('/mnt/isilon/marsh_single_unit/PythonEEG Data Bins')
+    output_folder = Path(__file__).parent.parent.resolve() / 'pipeline-wars'
+    animal_ids = ['A10 KO', 'F22 KO']
+
+    print("\nStarting pipeline execution...")
+    for animal_id in animal_ids:
+        ao = visualization.AnimalOrganizer(base_folder, animal_id, mode="concat", assume_from_number=True)
+        ao.convert_colbins_to_rowbins()
+        ao.convert_rowbins_to_rec()
+
+        start_time = time.time()
+        war = ao.compute_windowed_analysis(['rms'], exclude=['nspike', 'wavetemp'], multiprocess_mode='dask')
+        print(time.time() - start_time)
+        raise Exception("Stop here")
+        # war.to_pickle_and_json(output_folder / animal_id)
+
+
+
 """
-sbatch --mem 25G -c 20 -t 1:00:00 /mnt/isilon/marsh_single_unit/PythonEEG/notebooks/examples/pipeline-batch/pipeline-parallel-testing.sh 
+sbatch --mem 25G -c 4 -t 24:00:00 /mnt/isilon/marsh_single_unit/PythonEEG/notebooks/examples/pipeline-batch/pipeline-parallel-testing.sh
 """
