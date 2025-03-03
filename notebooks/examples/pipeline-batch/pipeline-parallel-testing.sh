3d07e9fdb16f4cdab6dd7b3d892e110f52168f6e
Joseph Dong <josephdong1000@gmail.com>
2025-03-03 14:14:42 -0500

    deleted testing pipeline files

--- notebooks/examples/pipeline-batch/pipeline-parallel-testing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-module load Python/3.10.8-GCCcore-12.2.0.lua
-cd /mnt/isilon/marsh_single_unit/PythonEEG
-source .venv-linux-3.10/bin/activate
-
-python -u /mnt/isilon/marsh_single_unit/PythonEEG/notebooks/examples/pipeline-batch/pipeline-parallel-testing.py
-
-echo "Pipeline finished."
