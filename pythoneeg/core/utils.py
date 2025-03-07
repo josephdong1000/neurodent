333340f2706beb99945345acd6d947e89aa7ff99
Joseph Dong <josephdong1000@gmail.com>
2025-03-07 18:44:26 -0500

    Add utility classes for temporary file handling and output suppression
    - Implement _CustomNamedTemporaryFile to handle cross-platform temporary file creation
    - Create _HiddenPrints context manager to suppress stdout when needed
    - Add necessary imports for file and system operations

--- pythoneeg/core/utils.py
+++ pythoneeg/core/utils.py
@@ -1,4 +1,7 @@
 # Standard library imports
+import os
+import sys
+import tempfile
 import re
 from datetime import datetime
 from pathlib import Path
@@ -38,3 +41,44 @@ def filepath_to_index(filepath) -> int:
     fname = re.split(r'\D+', fname)
     fname = list(filter(None, fname))
     return int(fname[-1])
+
+
+class _CustomNamedTemporaryFile:
+    """
+    This custom implementation is needed because of the following limitation of tempfile.NamedTemporaryFile:
+
+    > Whether the name can be used to open the file a second time, while the named temporary file is still open,
+    > varies across platforms (it can be so used on Unix; it cannot on Windows NT or later).
+    """
+    def __init__(self, mode='wb', delete=True):
+        self._mode = mode
+        self._delete = delete
+
+    def __enter__(self):
+        # Generate a random temporary file name
+        file_name = os.path.join(tempfile.gettempdir(), os.urandom(24).hex())
+        # Ensure the file is created
+        open(file_name, "x").close()
+        # Open the file in the given mode
+        self._tempFile = open(file_name, self._mode)
+        return self._tempFile
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._tempFile.close()
+        if self._delete:
+            os.remove(self._tempFile.name)
+
+
+class _HiddenPrints:
+    def __init__(self, silence=True) -> None:
+        self.silence = silence
+
+    def __enter__(self):
+        if self.silence:
+            self._original_stdout = sys.stdout
+            sys.stdout = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.silence:
+            sys.stdout.close()
+            sys.stdout = self._original_stdout
