92401569fd895870999fc64a676fe8569b3a92c1
Joseph Dong <josephdong1000@gmail.com>
2025-03-31 15:47:19 -0400

    clean up freqbands syntax in constants

--- pythoneeg/constants.py
+++ pythoneeg/constants.py
@@ -46,8 +46,8 @@ FREQ_BANDS = {'delta' : (0.1, 4),
 BAND_NAMES = [k for k,_ in FREQ_BANDS.items()]
 
 FREQ_BAND_TOTAL = (0.1, 50)
-FREQ_MINS = [v[0] for k,v in FREQ_BANDS.items()]
-FREQ_MAXS = [v[1] for k,v in FREQ_BANDS.items()]
+FREQ_MINS = [v[0] for _,v in FREQ_BANDS.items()]
+FREQ_MAXS = [v[1] for _,v in FREQ_BANDS.items()]
 LINE_FREQ = 60
 
 SORTING_PARAMS = {
