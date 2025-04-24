6ecaa4b7e7f20798ee52b1ae937c62c8084e8c77
Joseph Dong <josephdong1000@gmail.com>
2025-04-24 14:23:01 -0400

    Add apfig.py figure-generating pipeline

---
+++ notebooks/examples/pipeline-apfig.py
@@ -0,0 +1,55 @@
+import os
+import sys
+from pathlib import Path
+import time
+import tempfile
+import logging
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import mne
+from dask_jobqueue import SLURMCluster
+from dask.distributed import Client
+
+packageroot = Path('/home/dongjp/source-code/PyEEG').resolve()
+sys.path.append(str(packageroot))
+
+from pythoneeg import core
+from pythoneeg import visualization
+from pythoneeg import constants
+
+core.set_temp_directory('/scr1/users/dongjp')
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO, stream=sys.stdout, force=True)
+logger = logging.getLogger()
+
+animal_ids = ['A5', 'A10', 'F22', 'G25', 'G26', 'N21', 'N22', 'N23', 'N24', 'N25']
+base_folder = Path('/home/dongjp/source-code/PyEEG/notebooks/tests/test-wars-full/').resolve()
+save_folder = Path('/home/dongjp/Downloads/4-23-25_no-Z-score').resolve()
+if not save_folder.exists():
+    save_folder.mkdir(parents=True)
+
+for animal_id in animal_ids:
+
+    # SECTION load in windowed analysis results
+    war = visualization.WindowAnalysisResult.load_pickle_and_json(base_folder / f'{animal_id}')
+
+    # SECTION load into AP
+    save_path = save_folder / animal_id
+    if not save_path.exists():
+        save_path.mkdir(parents=True)
+    ap = visualization.AnimalPlotter(war, save_fig=True, save_path=save_path / animal_id)
+
+    # SECTION plot
+    ap.plot_coherecorr_spectral(figsize=(20, 5), score_type='none')
+    ap.plot_psd_histogram(figsize=(10, 4), avg_channels=True, plot_type='loglog')
+    ap.plot_psd_spectrogram(figsize=(20, 4), mode='none')
+
+    # SECTION cleanup WAR
+    del war
+
+
+"""
+sbatch --mem 20G -c 4 -t 24:00:00 ./notebooks/examples/pipeline.sh ./notebooks/examples/pipeline-apfig.py
+"""
