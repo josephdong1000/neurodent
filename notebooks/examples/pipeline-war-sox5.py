3a635ed261ecef7b75467fe4dd6b34bf4af94519
Joseph Dong <josephdong1000@gmail.com>
2025-04-25 12:51:31 -0400

    Fix sox5 pipeline

--- notebooks/examples/pipeline-war-sox5.py
+++ notebooks/examples/pipeline-war-sox5.py
@@ -14,9 +14,8 @@ from dask_jobqueue import SLURMCluster
 from dask.distributed import Client
 from django.utils.text import slugify
 
-
-packageroot = Path('/home/dongjp/source-code/PyEEG').resolve()
-sys.path.append(str(packageroot))
+base_folder = Path('/mnt/isilon/marsh_single_unit/PythonEEG')
+sys.path.append(str(base_folder))
 
 from pythoneeg import core
 from pythoneeg import visualization
@@ -26,11 +25,11 @@ core.set_temp_directory('/scr1/users/dongjp')
 
 # SECTION 1: Set up clusters
 cluster_window = SLURMCluster(
-        cores=8,
+        cores=9,
         memory='100GB',
         walltime='24:00:00',
         interface=None,
-        scheduler_options={'interface': 'eth1'},
+        scheduler_options={'interface': 'eth1'}, # Look at `nmcli dev status` to find the correct interface
         job_extra_directives=['--output=/dev/null',
                              '--error=/dev/null']
     )
@@ -41,22 +40,22 @@ cluster_spike = SLURMCluster(
         processes=1,
         walltime='12:00:00',
         interface=None,
-        scheduler_options={'interface': 'eth1'},
+        scheduler_options={'interface': 'eth1'}, # Look at `nmcli dev status` to find the correct interface
         job_extra_directives=['--output=/dev/null',
                              '--error=/dev/null']
     )
 print(f"\n\n\tcluster_spike.dashboard_link: {cluster_spike.dashboard_link}\n\n")
 cluster_window.scale(10)
-cluster_spike.scale(12)
+cluster_spike.scale(10)
 cluster_window.wait_for_workers(10)
-cluster_spike.wait_for_workers(12)
+cluster_spike.wait_for_workers(10)
 
 
 # SECTION 2: Compute windowed analysis
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG, stream=sys.stdout, force=True)
 logger = logging.getLogger()
 
-with open(Path('./notebooks/tests/sox5.json').resolve(), 'r') as f:
+with open(base_folder / 'notebooks' / 'tests' / 'sox5.json', 'r') as f:
     data = json.load(f)
 data_parent_folder = Path(data['data_parent_folder'])
 constants.GENOTYPE_ALIASES = data['GENOTYPE_ALIASES']
@@ -70,23 +69,23 @@ for data_folder, animal_ids in data_folders_to_animal_ids.items():
                                     assume_from_number=True,
                                     skip_days=['bad'])
         with Client(cluster_window) as client:
-            client.upload_file(str(packageroot / 'pythoneeg.zip'))
+            client.upload_file(str(base_folder / 'pythoneeg.zip'))
             
             ao.convert_colbins_to_rowbins(overwrite=True, multiprocess_mode='dask')
             ao.convert_rowbins_to_rec(multiprocess_mode='dask') # paralleization breaks if not enough memory
             war = ao.compute_windowed_analysis(['all'], multiprocess_mode='dask')
 
         with Client(cluster_spike) as client:
-            client.upload_file(str(packageroot / 'pythoneeg.zip'))
+            client.upload_file(str(base_folder / 'pythoneeg.zip'))
             
             sars = ao.compute_spike_analysis(multiprocess_mode='dask')
             for sar in sars:
-                sar.save_fif_and_json(Path(f'./notebooks/tests/test-sars-full/{data_folder} {slugify(sar.animal_day, allow_unicode=True)}').resolve(), overwrite=True) # animal_day not unique for Sox5 rec sessions, so add bin_folder_name
+                sar.save_fif_and_json(base_folder / 'notebooks' / 'tests' / 'test-sars-full' / f'{data_folder} {slugify(sar.animal_day, allow_unicode=True)}', overwrite=True) # animal_day not unique for Sox5 rec sessions, so add bin_folder_name
             war.read_sars_spikes(sars)
             
-        war.save_pickle_and_json(Path(f'./notebooks/tests/test-wars-full/{data_folder} {animal_id}').resolve())
+        war.save_pickle_and_json(base_folder / 'notebooks' / 'tests' / 'test-wars-full' / f'{data_folder} {animal_id}')
         wars.append(war)
 
 """
-sbatch --mem 100G -c 5 -t 48:00:00 ./notebooks/examples/pipeline.sh ./notebooks/examples/pipeline-war-sox5.py
+sbatch --mem 200G -c 4 -t 48:00:00 /mnt/isilon/marsh_single_unit/PythonEEG/notebooks/examples/pipeline.sh /mnt/isilon/marsh_single_unit/PythonEEG/notebooks/examples/pipeline-war-sox5.py
 """
