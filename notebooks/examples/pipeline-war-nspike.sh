dd1006c73055c9abc55cdbec51b857992851364b
Joseph Dong <josephdong1000@gmail.com>
2025-05-15 19:56:27 -0400

    Remove obsolete pipelines, package zip files, requirements txt, testing notebooks

--- notebooks/examples/pipeline-war-nspike.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-module load Python/3.10.8-GCCcore-12.2.0.lua
-cd ~/source-code/PyEEG
-source .venv/bin/activate
-
-python -u ./notebooks/examples/pipeline-war-nspike.py
-
-echo "Pipeline finished."
