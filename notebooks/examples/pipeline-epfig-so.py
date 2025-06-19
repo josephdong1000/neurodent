827ae2967d8d57b7842c020861c1e62219e86efc
Joseph Dong <josephdong1000@gmail.com>
2025-06-18 20:45:13 -0400

    Create bad channels dict, change save folder

--- notebooks/examples/pipeline-epfig-so.py
+++ notebooks/examples/pipeline-epfig-so.py
@@ -22,7 +22,7 @@ logger = logging.getLogger()
 
 base_folder = Path("/mnt/isilon/marsh_single_unit/PythonEEG")
 load_folder = base_folder / "notebooks" / "tests" / "test-wars-sox5-3"
-save_folder = base_folder / "notebooks" / "tests" / "test-wars-sox5-collapsed-3"
+save_folder = base_folder / "notebooks" / "tests" / "test-wars-sox5-collapsed-4"
 # animal_ids = ['A5', 'A10', 'F22', 'G25', 'G26', 'N21', 'N22', 'N23', 'N24', 'N25']
 # animal_ids = ['A5', 'A10']
 animal_ids = [p.name for p in load_folder.glob("*") if p.is_dir()]
@@ -35,6 +35,74 @@ animal_ids = [p.name for p in load_folder.glob("*") if p.is_dir()]
 # ]
 bad_animal_ids = []
 animal_ids = [animal_id for animal_id in animal_ids if animal_id not in bad_animal_ids]
+war_bad_channels = {
+    # 'FHet' : {
+    #     'A-F5' : ['LAud', 'LVis', 'LBar'],
+    #     'A-F6' : ['LAud', 'LVis', 'LBar'],
+    #     'D-F8' : ['LAud', 'LVis'],
+    #     'G-F4' : ['LAud', 'LVis'],
+    # }
+    'FWT' : {
+        'C-F2' : ['RVis', 'RAud'],
+        'B-F3' : ['RVis', 'RAud'],
+        'D-F3' : ['LAud', 'LVis'],
+        'FWT_cohort4_group4' : ['LAud', 'LVis', 'LHip', 'RHip', 'RAud']
+    },
+    'FHet' : {
+        'A-F5' : ['LAud', 'LVis', 'LBar'],
+        'A-F6' : ['LAud', 'LVis', 'LBar'],
+        'D-F8': ['LAud', 'LVis'],
+        'G-F4': ['LAud', 'LVis'],
+        'I-F5' : ['LAud', 'RVis', 'RAud'],
+        'F-F6' : ['LAud', 'LVis', 'LBar', 'RVis'],
+        'FHET(1)_cohort4_group7' : ['LAud', 'LVis'],
+        'FHET(2)_cohort4_group7' : ['LAud', 'LVis', 'RAud'],
+        'F4_FHET_cohort5_group1' : ['LAud', 'LVis'],
+    },
+    'FMut' : {
+        'C-F10': ['LAud', 'LVis'],
+        'C-F9': ['LAud', 'LBar', 'RAud'],
+        'FMUT_cohort4_group6': ['LAud', 'LVis', 'LHip', 'LBar'],
+        'FMUT_cohort4_group5': ['LHip', 'RHip'],
+        'FMUT(2)_cohort4_group4': ['LVis', 'LHip', 'RHip'],
+        'FMUT_cohort4_group4': ['LAud', 'LHip', 'RBar', 'RHip', 'RAud'],
+        'FMUT_cohort10_group4': ['RAud'],
+        'FMUT(3)cohort10_group4': ['RAud'],
+        'F9_FMUT_cohort10_cage3': ['RAud']
+    },
+    'MWT': {
+        'B-M6': ['LAud', 'LVis', 'RAud'],
+        'C-M9': ['LAud', 'LVis', 'LBar'],
+        'C-M5': ['LAud', 'LVis', 'LHip', 'LBar', 'LMot', 'RMot', 'RBar', 'RHip', 'RVis', 'RAud'],
+        'A-M4': ['LAud', 'LVis', 'LBar'],
+        'M7_MWT_cohort6_group1': ['LVis', 'LBar', 'RMot', 'RBar', 'RHip', 'RVis', 'RAud'],
+        'MWT_cohort4_group3': ['LMot'],
+        'M10_MWT_cohort4_group2': ['LAud', 'LVis', 'LHip', 'LBar', 'LMot', 'RMot', 'RBar', 'RHip', 'RVis', 'RAud'],
+        'M2_group9_cage3': ['LAud', 'LVis', 'LHip', 'LBar', 'LMot', 'RMot', 'RBar', 'RHip', 'RVis', 'RAud']
+    },
+    'MHet': {
+        'E-M1': ['LAud', 'LVis'],
+        'B-M3': ['LAud', 'LVis'],
+        'C-M3': ['LAud', 'LVis'],
+        'D-M3': ['LAud', 'LVis'],
+        'C-M2': ['LAud', 'LVis'],
+        'MHET_cohort4_group3': ['RAud'],
+        'M3_group9_cage4': ['RAud']
+    },
+    'MMut':{
+        'B-M2': ['LHip'],
+        'C-M8': ['LAud', 'LVis'],
+        'C-M5': ['LAud', 'LBar'],
+        'D-M5': ['LAud', 'LVis', 'LHip', 'LBar', 'LMot', 'RMot', 'RBar', 'RHip', 'RVis', 'RAud'],
+        'MMUT(1)_cohort6_group1': ['LAud', 'LVis'],
+        'MMUT(2)_cohort6_group1': ['LAud', 'LVis', 'LHip', 'LBar', 'LMot', 'RMot', 'RBar', 'RHip', 'RVis', 'RAud'],
+        'OLDMMT_cohort4_group4': ['LAud', 'LVis', 'LHip', 'LBar', 'LMot', 'RMot', 'RBar', 'RHip', 'RVis', 'RAud'],
+        'MMT_cohort4_group3': ['LAud', 'LVis', 'LMot'],
+        'MMUT(2)_cohort10_group4': ['LVis'],
+        'M8_MMT_group10_cage1': ['LAud', 'LVis', 'LHip', 'LBar', 'RAud']
+    }
+}
+
 if not save_folder.exists():
     save_folder.mkdir(parents=True)
 
