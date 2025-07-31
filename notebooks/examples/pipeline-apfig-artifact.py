9be528949c3d7cdc1625f26b8c19f84f43e3af76
Joseph Dong <josephdong1000@gmail.com>
2025-07-30 20:28:48 -0400

    Create artifact pipeline script

---
+++ notebooks/examples/pipeline-apfig-artifact.py
@@ -0,0 +1,59 @@
+import logging
+import sys
+from multiprocessing import Pool
+from pathlib import Path
+
+from tqdm import tqdm
+
+from pythoneeg import constants, core, visualization
+
+core.set_temp_directory("/scr1/users/dongjp")
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO, stream=sys.stdout, force=True
+)
+logger = logging.getLogger()
+
+base_folder = Path("/mnt/isilon/marsh_single_unit/PythonEEG")
+load_folder = base_folder / "notebooks" / "tests" / "test-wars-sox5-7"
+save_folder = Path("/home/dongjp/Downloads/7-30 artifact figs")
+if not save_folder.exists():
+    save_folder.mkdir(parents=True, exist_ok=True)
+
+animal_ids = [p.name for p in load_folder.glob("*") if p.is_dir()]
+bad_animal_ids = [
+    "013122_cohort4_group7_2mice both_FHET FHET(2)",
+    "012322_cohort4_group6_3mice_FMUT___MMUT_MWT MHET",
+    "012322_cohort4_group6_3mice_FMUT___MMUT_MWT MMUT",
+    "011622_cohort4_group4_3mice_MMutOLD_FMUT_FMUT_FWT OLDMMT",
+    "011322_cohort4_group3_4mice_AllM_MT_WT_HET_WT M3",
+    "012322_cohort4_group6_3mice_FMUT___MMUT_MWT FHET",
+]
+animal_ids = [p for p in animal_ids if p not in bad_animal_ids]
+
+def plot_animal(animal_id):
+    logger.info(f"Plotting {animal_id}")
+    war = visualization.WindowAnalysisResult.load_pickle_and_json(load_folder / f"{animal_id}")
+    if war.genotype == "Unknown":
+        logger.info(f"Skipping {animal_id} because genotype is Unknown")
+        return None
+
+    # save_path = save_folder / animal_id
+    # if not save_path.exists():
+    #     save_path.mkdir(parents=True)
+    save_path = save_folder
+
+    ap = visualization.AnimalPlotter(war, save_fig=True, save_path=save_path / f"{animal_id}")
+    ap.plot_artifact_diagnosis(features="rms", figsize=(10, 5))
+    ap.plot_artifact_diagnosis(features="zcohere", figsize=(10, 5))
+
+    return animal_id
+
+
+with Pool(10) as pool:
+    list(tqdm(pool.imap(plot_animal, animal_ids), total=len(animal_ids), desc="Processing animals"))
+
+
+"""
+sbatch --mem 300G -c 11 -t 24:00:00 ./notebooks/examples/pipeline.sh ./notebooks/examples/pipeline-apfig-artifact.py
+"""
